842e3bf10d2d48a9c616c87c94c057d9d8bb27f4
L200184090 <44854016+L200184090@users.noreply.github.com>
2019-10-09 20:05:24 +0700

    Prak_4 | Modul 5
    L200184090 M. Rifqy Fauzy

---
+++ Modul_5/lat1/Lat1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lat1;
+
+/**
+ *
+ * @author USER
+ */
+public class Lat1 {
+    String nama = "M. Rifqy Fauzy";
+    String nim = "L200184090";
+    String alamat = "Gonilan";
+
+    public Lat1() {
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+        System.out.println("Alamat : " + alamat);
+    }
+}
